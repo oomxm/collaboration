f55751f1be2cfcebce135fd63197a2c4e942e048
oomxm <oomxm@naver.com>
2019-05-24 13:37:12 +0900

    프로젝트 개시

---
+++ TeamProject/src/com/kh/semi/Test.java
@@ -0,0 +1,8 @@
+package com.kh.semi;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("프로젝트 개시~!~!~!~!~!~!");
+		System.out.println("팀원 하시는 분들도 잘 보새용 저장하새용");
+	}
+}
