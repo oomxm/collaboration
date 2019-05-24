becc878f65e435b77a1fc64c1d0c8484345599af
oomxm <oomxm@naver.com>
2019-05-24 14:27:02 +0900

    merging & 커밋 했읍니다

--- TeamProject/src/com/kh/semi/NeooungTest.java
+++ TeamProject/src/com/kh/semi/NeooungTest.java
@@ -4,6 +4,7 @@ public class NeooungTest {
 	
 	public static void main(String[] args) {
 		System.out.println("하욤하욤 서여이엥요~~~ merge해주세욥ㅂㅂ");
+		System.out.println("선생님 루미큐브 입장료 백원 해 주 세요");
 	}
 
 }
--- TeamProject/src/com/kh/semi/Team1.java
+++ TeamProject/src/com/kh/semi/Team1.java
@@ -4,7 +4,7 @@ public class Team1 {
 
 	public static void main(String[] args) {
 	System.out.print("이걸 확인할 수 있는건가요? ");
-
+	System.out.println("넹!");
 	}
 
 }
